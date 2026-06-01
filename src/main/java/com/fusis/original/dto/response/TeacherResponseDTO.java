7371f35a5ebc9fe3d3c6dfd70afe2328b10729ba
uzmelih <uzmelih14@gmail.com>
2026-06-01 18:56:33 +0300

    FUS-26 DTO (Data Transfer Object) yapısı ile sadece gerekli verilerin API üzerinden sunulması.

---
+++ src/main/java/com/fusis/original/dto/response/TeacherResponseDTO.java
@@ -0,0 +1,11 @@
+package com.fusis.original.dto.response;
+
+import lombok.Data;
+
+@Data
+public class TeacherResponseDTO {
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+}
---
+++ src/main/java/com/fusis/original/dto/response/UserResponseDTO.java
@@ -0,0 +1,11 @@
+package com.fusis.original.dto.response;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDTO {
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+}
